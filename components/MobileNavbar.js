7865eede20126c3e44c636e905d914fd89ade5c5
pablo <pfernandezrecaman@gmail.com>
2024-01-08 13:21:20 +0100

    estilando navbar y index

--- components/MobileNavbar.js
+++ components/MobileNavbar.js
@@ -1,13 +1,23 @@
 export async function createNavbar() {
-    const menuContainer = document.getElementById('mobile-menu');
+    const menuContainer = document.getElementById('mobile-nav');
     const svgFiles = ['index.svg', 'slopes.svg', 'weather.svg', 'webcams.svg'];
+
+    const navList = document.createElement('ul');
+    menuContainer.appendChild(navList);
 
     for (const file of svgFiles) {
         try {
             const baseName = file.split('.')[0];
             const svgContent = await fetch(`/assets/icons/menu/${file}`).then(res => res.text());
-            // Aquí agregamos un enlace que apunta a la página correspondiente.
-            menuContainer.innerHTML += `<a href="${baseName}.html"><button><span id="${baseName}-icon">${svgContent}</span></button></a>`;
+
+            const listItem = document.createElement('li');
+            const button = document.createElement('button');
+            button.innerHTML = svgContent;
+            button.setAttribute('aria-label', baseName);
+            button.onclick = () => location.href = `${baseName}.html`;
+
+            listItem.appendChild(button);
+            navList.appendChild(listItem);
         } catch (error) {
             console.error('Error loading SVG:', error);
         }
